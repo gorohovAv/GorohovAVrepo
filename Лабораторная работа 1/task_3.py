456b781ec03e2ab9d2acf03127c8907b23bf0810
gorohovAv <153224515+gorohovAv@users.noreply.github.com>
2023-12-07 23:02:12 +0300

    Delete Лабораторная работа 1 directory

--- Лабораторная работа 1/task_3.py
+++
@@ -1,7 +0,0 @@
-list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-number_of_players = len(list_players)
-team_1 = list_players[:3:1]
-team_2 = list_players[3::1]
-print(team_1)
-print(team_2)
-# TODO Разделите участников на две команды
